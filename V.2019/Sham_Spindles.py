1fc240159782d8fb37d1dec029e31fa9d99a5a24
RobertoFelipeSG <38394703+RobertoFelipeSG@users.noreply.github.com>
2022-09-27 16:34:48 -0400

    Add files via upload

---
+++ V.2019/Sham_Spindles.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import numpy as np
+from scipy import signal
+import simpleaudio as sa
+import RPi.GPIO as GPIO
+
+def Sham_Spindles():
+    
+    pinNum = 22
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pinNum,GPIO.OUT)
+    GPIO.output(pinNum,False)
+        
+    sample_rate = 44100
+
+    #audio = np.load('spindles_4min.npy')
+    audio = np.load('spindles_sham.npy')
+
+    # normalize to 16-bit range
+    audio *= 32767 / np.max(np.abs(audio))
+    # convert to 16-bit data
+    audio = audio.astype(np.int16)
+
+    # start playback
+    play_obj = sa.play_buffer(audio, 1, 2, sample_rate)
+
+    # wait for playback to finish before exiting
+    #play_obj.wait_done()
---
+++ V.2019/SlowOsc_rtDCS.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import numpy as np
+from scipy import signal
+import simpleaudio as sa
+import RPi.GPIO as GPIO
+
+def SlowOsc_rtDCS():
+    
+    pinNum = 22
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pinNum,GPIO.OUT)
+    GPIO.output(pinNum,True)
+        
+    sample_rate = 44100
+
+    audio = np.load('slowosc_4min.npy')
+    #audio = np.load('sham_spindles.npy')
+
+    # normalize to 16-bit range
+    audio *= 32767 / np.max(np.abs(audio))
+    # convert to 16-bit data
+    audio = audio.astype(np.int16)
+
+    # start playback
+    play_obj = sa.play_buffer(audio, 1, 2, sample_rate)
+
+    # wait for playback to finish before exiting
+    #play_obj.wait_done()
+    
+    
